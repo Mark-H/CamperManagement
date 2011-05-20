2f9f0ec292b3a1728da574618a6d6e42ef13923d
Mark-H <hamstra.mark@gmail.com>
2011-05-21 01:26:43 +0200

    Split up files into page.index.js into smaller files for easier development

---
+++ assets/components/campermanagement/js/mgr/widgets/index.campers.grid.js
@@ -0,0 +1,77 @@
+/*
+Index Campers & Caravans Grid
+ */
+CamperMgmt.indexGrid = function(config) {
+    config = config || {};
+    Ext.applyIf(config,{
+		url: CamperMgmt.config.connectorUrl,
+		id: 'index-grid',
+		baseParams: { action: 'mgr/index/getcampers' },
+		fields: ['id','brand','type','plate','car','engine','manufactured','beds','weight','mileage','periodiccheck','remarks','price','status','keynr','owner','brand','options'],
+		paging: true,
+		autosave: false,
+		remoteSort: true,
+		items: [{
+			xtype: 'tbbutton',
+			text: 'Nieuwe camper toevoegen',
+			handler: function(btn,e) {
+				/*if (typeof newEventWindow == 'undefined') {
+					newEventWindow = MODx.load({
+						xtype: 'eventmanager-new-event',
+						listeners: {
+							'success': function() {
+								newEventWindow.hide();
+								this.reload();
+							}
+						}
+					});
+				}
+				newEventWindow.show(e.target);*/
+                return true;
+			}
+		}]
+		,columns: [{
+			header: '#'
+			,dataIndex: 'id'
+			,sortable: true
+			,width: 3
+			,hidden: false
+		},{
+			header: 'Key'
+			,dataIndex: 'keynr'
+			,sortable: true
+			,width: 5
+			,hidden: false
+		},{
+			header: 'Merk',
+			dataIndex: 'brand',
+		    sortable: true,
+			width: 14
+		},{
+			header: 'Type',
+			dataIndex: 'type',
+			sortable: true,
+			width: 20
+		},{
+			header: 'Eigenaar',
+			dataIndex: 'owner',
+    		sortable: true,
+			width: 16
+		},{
+			header: 'Prijs',
+			dataIndex: 'price',
+			sortable: true,
+			width: 18
+		}]
+		,listeners: {
+			'cellcontextmenu': function(grid, row, col, eventObj){
+				return true;
+			}
+		}
+
+
+    });
+    CamperMgmt.indexGrid.superclass.constructor.call(this,config);
+};
+Ext.extend(CamperMgmt.indexGrid,MODx.grid.Grid);
+Ext.reg('campermgmt-grid-index',CamperMgmt.indexGrid);
--- core/components/campermanagement/controllers/index.php
+++ core/components/campermanagement/controllers/index.php
@@ -8,8 +8,13 @@ Ext.onReady(function() {
     CamperMgmt.config = '.$modx->toJSON($campermgmt->config).';
 });
 </script>');
+// @TODO: In the actual package, make sure these are minified and/or combined into one or a few files.
 $modx->regClientStartupScript($campermgmt->config['jsUrl'].'mgr/campermanagement.js');
 $modx->regClientStartupScript($campermgmt->config['jsUrl'].'mgr/page.index.js');
+$modx->regClientStartupScript($campermgmt->config['jsUrl'].'mgr/widgets/index.campers.grid.js');
+$modx->regClientStartupScript($campermgmt->config['jsUrl'].'mgr/widgets/index.owners.grid.js');
+$modx->regClientStartupScript($campermgmt->config['jsUrl'].'mgr/widgets/index.options.grid.js');
+$modx->regClientStartupScript($campermgmt->config['jsUrl'].'mgr/widgets/index.brands.grid.js');
 
 return '<div id="campermanagement"></div>';
 ?>
