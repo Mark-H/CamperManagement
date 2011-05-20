d721b23a791c4d2e1cfa20c89c82ed47af944381
Mark-H <hamstra.mark@gmail.com>
2011-05-21 01:28:12 +0200

    Add basic cmowners snippet

---
+++ core/components/campermanagement/elements/snippets/snippet.cmowners.php
@@ -0,0 +1,22 @@
+<?php
+
+$start = $modx->getOption('start',$scriptProperties,0);
+$limit = $modx->getOption('limit',$scriptProperties,20);
+$sort = $modx->getOption('sort',$scriptProperties,'lastname');
+$dir = $modx->getOption('dir',$scriptProperties,'asc');
+
+$results = array();
+
+$query = $modx->newQuery('cmOwner');
+$query->sortby($sort,$dir);
+
+$count = $modx->getCount('cmOwner',$query);
+
+$query->limit($limit,$start);
+$owners = $modx->getCollection('cmOwner',$query);
+
+foreach ($owners as $owner) {
+    $results[] = $owner->toArray();
+}
+
+return '.<pre>'.print_r($results).'</pre>';
