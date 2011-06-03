b4b6ec08bdbb8cb953129fb5aebe27354962bd9f
Mark-H <hamstra.mark@gmail.com>
2011-06-04 00:00:41 +0200

    Bump to 0.5.0-rc1

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -17,7 +17,7 @@ set_time_limit(0);
 /* define package */
 define('PKG_NAME','CamperManagement');
 define('PKG_NAME_LOWER',strtolower(PKG_NAME));
-define('PKG_VERSION','0.3.0');
+define('PKG_VERSION','0.5.0');
 define('PKG_RELEASE','rc1');
 
 $root = dirname(dirname(__FILE__)).'/';
