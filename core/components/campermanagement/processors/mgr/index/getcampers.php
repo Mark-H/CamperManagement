7b4ac201de006d2bca2958260f9d5ed5ec5a1109
Mark-H <hamstra.mark@gmail.com>
2011-06-03 23:32:33 +0200

    Make sure processor uses translated status names

--- core/components/campermanagement/processors/mgr/index/getcampers.php
+++ core/components/campermanagement/processors/mgr/index/getcampers.php
@@ -36,7 +36,7 @@ $count = $modx->getCount('cmCamper',$query);
 
 $query->limit($limit,$start);
 
-$statusnames = array($modx->lexicon('campermgmt.status0'),$modx->lexicon('campermgmt.status1'),$modx->lexicon('campermgmt.status2'),$modx->lexicon('campermgmt.status3'),$modx->lexicon('campermgmt.status4'),$modx->lexicon('campermgmt.status5'));
+$statuses = array($modx->lexicon('campermgmt.status0'),$modx->lexicon('campermgmt.status1'),$modx->lexicon('campermgmt.status2'),$modx->lexicon('campermgmt.status3'),$modx->lexicon('campermgmt.status4'),$modx->lexicon('campermgmt.status5'));
 
 $campers = $modx->getCollectionGraph('cmCamper','{ "Brand":{}, "Owner": {} }',$query);
 
